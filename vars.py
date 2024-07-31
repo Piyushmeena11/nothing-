0cfc0699e3151160377f860fda73a58813d724b5
bhuriya12 <162349127+bhuriya12@users.noreply.github.com>
2024-07-31 14:04:44 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -1,4 +1,4 @@
 api_id = 26451206
 api_hash = "32984406271d6f3945bb536671b143a7"
-bot_token = "7420653099:AAGOp-u6Rx7tfiMLIvkxzNLJnmXJH_fMP1Q"
+bot_token = "7471442735:AAGyjSHNOoZMwNlWx1Ohx7Ue1y4I8_v6bvc"
 # Bot Username = "allinone"
